2259173c7ab2432023c3184b70c9a3c9ce103012
fdennig <fdennig@gmail.com>
2019-12-10 11:58:13 +0100

    deleted some legacy files from the package

--- examples/main.jl
+++ examples/main.jl
@@ -7,3 +7,9 @@ m = get_model()
 run(m)
 
 explore(m)
+
+# say you want to change some parameter
+#like the discount rate
+m[:welfare,:rho] = 1.5
+#or the mitigation fraction, this is how we input the control variable in the optimisation
+m[:emissions,:MIU] = 0.5*ones(60,12)
